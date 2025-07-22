bc6ead1e7d4cf3e36491df6bc2cf7f70ad531223
Caesar-bit <danielbonsu740@gmail.com>
2025-07-22 15:36:33 +0000

    Add statistics page fetching data via API

---
+++ Pages/Statistics/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentGradeTrackingSystem.Pages.Statistics
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
